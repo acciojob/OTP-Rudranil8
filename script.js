14ac115680ae19926ab21f0c0542455af992af13
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-04-21 02:47:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 //your JS code here. If required.
+// var inputs = document.getElementsByClassName('code');
+
+// for (var i = 0; i < inputs.length; i++) {
+//   inputs[i].addEventListener('keyup', function(e) {
+//     if (this.value && this.nextElementSibling) {
+//       this.nextElementSibling.focus();
+//     }
+//   });
+  
+//   inputs[i].addEventListener('keydown', function(e) {
+//     if (e.key === "Backspace" && this.previousElementSibling) {
+//       this.previousElementSibling.focus();
+//     }
+//   });
+// }
 var inputs = document.getElementsByClassName('code');
 
 for (var i = 0; i < inputs.length; i++) {
@@ -10,6 +25,7 @@ for (var i = 0; i < inputs.length; i++) {
   
   inputs[i].addEventListener('keydown', function(e) {
     if (e.key === "Backspace" && this.previousElementSibling) {
+      this.value = '';
       this.previousElementSibling.focus();
     }
   });
