6e9039b932ffcf1a55dfc544d4fe1fae9cca27c6
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-04-21 02:52:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,19 +14,21 @@
 //     }
 //   });
 // }
-var inputs = document.getElementsByClassName('code');
+window.onload = function() {
+  var inputs = document.getElementsByClassName('code');
 
-for (var i = 0; i < inputs.length; i++) {
-  inputs[i].addEventListener('keyup', function(e) {
-    if (this.value && this.nextElementSibling) {
-      this.nextElementSibling.focus();
-    }
-  });
-  
-  inputs[i].addEventListener('keydown', function(e) {
-    if (e.key === "Backspace" && this.previousElementSibling) {
-      this.value = '';
-      this.previousElementSibling.focus();
-    }
-  });
+  for (var i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener('keyup', function(e) {
+      if (this.value && this.nextElementSibling) {
+        this.nextElementSibling.focus();
+      }
+    });
+    
+    inputs[i].addEventListener('keydown', function(e) {
+      if (e.key === "Backspace" && this.previousElementSibling) {
+        this.value = '';
+        this.previousElementSibling.focus();
+      }
+    });
+  }
 }
